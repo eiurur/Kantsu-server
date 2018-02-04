5ecaad1898d84cf41b070d8385c0e3a3f2d96ff7
eiurur <sigsig_kbeatodenmania@yahoo.co.jp>
2018-02-04 23:02:45 +0900

    [Change] - recieve multiple requests

--- server/routes/v1/controller/EroterestController.js
+++ server/routes/v1/controller/EroterestController.js
@@ -14,35 +14,28 @@ module.exports = class EroterestController {
   // 外部リンクから動画リンクを抜き出して返す
   static async fetchMovieUrl(req, res) {
     try {
-      // const urls = decodeURIComponent(req.body.urls);
-      console.log(req.body);
-
       const eroterest_movie_page_pattern = /https:\/\/movie.eroterest.net\/page\/(\d+)\//;
-      const eroterestPageUrls = req.body.urls.filter(url => eroterest_movie_page_pattern.test(url));
+      if (!eroterest_movie_page_pattern.test(req.body.url)) {
+        throw new Error('invalid url');
+      }
 
-      const movies = await Promise.all(eroterestPageUrls.map(async (url) => {
-        if (!eroterest_movie_page_pattern.test(url)) {
-          throw new Error(`${url} is invalid url`);
+      // production環境ではキャッシングする
+      if (process.env.NODE_ENV === 'production') {
+        const reply = await redisClient.getAsync(req.body.url);
+        const cachedMovie = JSON.parse(reply);
+        if (cachedMovie !== null) {
+          console.log('caching!');
+          res.json({ movie: cachedMovie });
+          return;
         }
+      }
 
-        // production環境ではキャッシングする
-        if (process.env.NODE_ENV === 'production') {
-          const reply = await redisClient.getAsync(url);
-          const cachedMovie = JSON.parse(reply);
-          if (cachedMovie !== null) {
-            return cachedMovie;
-          }
-        }
+      const externalPageUrl = await EroterestController.fetchExternalUrl(req.body.url);
+      const scraper = new MovieLinkScraper(externalPageUrl);
+      const movie = await scraper.exec();
+      redisClient.set(req.body.url, JSON.stringify(movie));
 
-        const externalPageUrl = await EroterestController.fetchExternalUrl(url);
-        const scraper = new MovieLinkScraper(externalPageUrl);
-        const movie = await scraper.exec();
-        redisClient.set(url, JSON.stringify(movie));
-
-        return movie;
-      }));
-
-      res.json({ movies });
+      res.json({ movie });
     } catch (e) {
       res.status(400).send(e.message);
     }
