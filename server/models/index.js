4353b174ae4fe961e350910fe297e1adb50aebf9
eiurur <eiurur789@gmail.com>
2022-08-01 18:37:17 +0900

    update interface

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,12 @@
 const redis = require('redis');
 
 let redisClient = null;
-console.log(process.env);
+console.log(process.env.REDIS_URI);
 if (process.env.REDIS_URI) {
-  redisClient = redis.createClient(process.env.REDIS_URI);
+  console.log('connect url:', process.env.REDIS_URI);
+  redisClient = redis.createClient({ url: process.env.REDIS_URI });
 } else {
+  console.log('connect local redis');
   redisClient = redis.createClient(6379, 'redis');
 }
 if (process.env.REDIS_PASSWORD) {
